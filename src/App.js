a53d43cee8360a4b12eeb60f8c9d2421f58d805f
Kevin Reid <kevinnivek@me.com>
2023-01-25 16:15:30 -0500

    committing develop branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+
+class App extends Component {
+
+  render() {
+    // const myStyle={
+    //   backgroundImage: `url(${backgroundImage})`,
+    //   height:'100vh',
+    //   marginTop:'-40px',
+    //   fontSize: '30px',
+    //   backgroundSize: 'cover',
+    //   backgroundRepeat: 'no-repeat'
+    // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1> Development and Design by Kevin Reid <span className = "spanH1"> </span> </h1>
     </div>
   );
+}
 }
 
 export default App;
